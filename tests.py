35465cf13b5c98116632c38f69f07d266350f76d
Julian Simioni <julian.simioni@gmail.com>
2013-02-05 18:32:40 -0800

    Test one child one parent

--- tests.py
+++ tests.py
@@ -8,3 +8,10 @@ def test_insert():
 
     assert_equal(6, node.right.data)
     assert_equal(node, node.right.parent)
+
+def test_one_child_one_parent():
+    node = Node(6)
+    node.insert(3)
+    node.insert(5)
+
+    assert_equal(5, nextLargest(node.left))
