269794cd4d6902fbb8628eb1b62df79cd2bb5486
Julian Simioni <julian.simioni@gmail.com>
2013-02-05 18:38:54 -0800

    Test child and parent missing

--- tests.py
+++ tests.py
@@ -15,3 +15,21 @@ def test_one_child_one_parent():
     node.insert(5)
 
     assert_equal(5, nextLargest(node.left))
+
+def test_one_parent():
+    """
+    No child this time, so null values must be considered
+    """
+    node = Node(6)
+    node.insert(3)
+
+    assert_equal(6, nextLargest(node.left))
+
+def test_one_child():
+    """
+    No child this time, so null values must be considered
+    """
+    node = Node(6)
+    node.insert(7)
+
+    assert_equal(7, nextLargest(node))
