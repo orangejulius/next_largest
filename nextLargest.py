71835ec1de717cffbb3452695b218f81a4d85590
Julian Simioni <julian.simioni@gmail.com>
2013-02-05 18:21:15 -0800

    Create Node class with insert function

---
+++ nextLargest.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = self.parent = 0
+
+    def insert(self, data):
+        if data < self.data:
+            if self.left:
+                self.left.insert(data)
+            else:
+                self.left = Node(data)
+                self.left.parent = self
+        else:
+            if self.right:
+                self.right.insert(data)
+            else:
+                self.right = Node(data)
+                self.right.parent = self
